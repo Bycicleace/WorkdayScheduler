fb0065211e8acc395c9c5fb9d624bfd684a7afc6
kvammee <elliott.kvamme@gmail.com>
2021-09-23 11:23:48 -0500

    Added rows with styles and past/present/future colors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,37 +14,60 @@ var CreateTimeRows = function() {
         // This is the container for the rest
         var row = $("<div>");
         row.attr("id", "row-" + String(i));
+        row.addClass("row m-0 p-0");
 
         // This is the element for the hour
-        var time = $("<h2>");
-        time.addClass("col-1");
+        var time = $("<h4>");
+        time.addClass("col-1 text-end align-middle p-0 pt-2 m-0 hour");
         // Sets the value of this element to the hour, inclding AM or PM
         if (i <= 12) {
-            time.val(String(i) + "AM");
+            time.text(String(i) + "AM");
         } else {
-            time.val(String(i - 12) + "PM");
+            time.text(String(i - 12) + "PM");
         }
 
         // This is the element for the data/possible appointments
         var notes = $("<p>");
-        notes.addClass("col-10");
+        notes.addClass("col-10 p-0 m-0 description");
 
         // This is for the save button on the end.
         var save = $("<div>");
-        save.addClass("btn btn-info col-1");
+        save.addClass("bg-info d-flex justify-content-center align-items-center col-1 text-center text-white saveBtn");
+        save.html("<i class='fas fa-save'></i>")
 
         // Add each element to the row
-        row.appendChild(time);
-        row.appendChild(notes);
-        row.appendChild(save);
+        row.append(time);
+        row.append(notes);
+        row.append(save);
 
         // Add row to container
-        $("#container").appendChild(row);
+        $(".container").append(row);
     }
     
+}
+
+var UpdateTimeColors = function() {
+    var currentHour = moment().hour();
+    for (var i = 9; i <= 17; i++) {
+        var currentDescription = $("#row-" + String(i) + " p")
+        if (i === currentHour) {
+            currentDescription.removeClass("past");
+            currentDescription.removeClass("future");
+            currentDescription.addClass("present");
+        } else if (i < currentHour) {
+            currentDescription.removeClass("present");
+            currentDescription.removeClass("future");
+            currentDescription.addClass("past");
+        } else if (i > currentHour) {
+            currentDescription.removeClass("past");
+            currentDescription.removeClass("present");
+            currentDescription.addClass("future");
+        }
+    }
 }
 
 
 // This is what adds the current day to the page.
 DisplayNow();
 CreateTimeRows();
+UpdateTimeColors();
